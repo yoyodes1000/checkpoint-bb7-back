d2db7e78a3613da77c90e143a20081e340e32040
yoyodes1000 <yoyodes1000@gmail.com>
2024-06-16 12:43:17 +0200

    first commit

---
+++ src/main/java/com/checkpoint/bb7/Bb7Application.java
@@ -0,0 +1,13 @@
+package com.checkpoint.bb7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bb7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bb7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/checkpoint/bb7/Bb7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkpoint.bb7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bb7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
